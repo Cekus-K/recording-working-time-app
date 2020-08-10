912262974dfcf83929130dc01cd627c6e86de150
Kamil Cekus <cekuskamil@gmail.com>
2020-08-10 12:13:53 +0200

    init commit

---
+++ src/main/java/pl/cekus/antologicproject/AntologicProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.cekus.antologicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntologicProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntologicProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/cekus/antologicproject/AntologicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cekus.antologicproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntologicProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
